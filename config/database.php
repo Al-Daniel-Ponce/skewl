143de7a1d0c262fa759ff379de7f7487cb9251e3
Nigga Pogi <solarbro8@gmail.com>
2026-04-25 21:36:21 +0800

    Update database.php and database.sql

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
-//database dtails
+
 $dbhost = "localhost:8080";
-$dbname = "comshop";
+$dbname = "shopp";
 $username = "root";
-$password = ""; //empty sa ni.. kay ambot
+$password = ""; 
 
 $dsn = "mysql:dbhost=$dbhost;dbname=$dbname;charset=utf8mb4";
 
-//pdo object
+
 
 $pdo = new PDO($dsn, $username, $password);
 //this file is for connecting to the database, and we will include this file in our products.php file to use the $pdo object for database operations. 
